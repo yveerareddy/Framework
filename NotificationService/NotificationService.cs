690536ea5d143073baba558830c1ae2673b46a80
yveerareddy <yveerareddy@gmail.com>
2017-09-02 12:55:55 +0100

    notification service

---
+++ NotificationService/NotificationService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationService
+{
+    public interface ISubscriptionService
+    {
+        Task SubscribeAsync<T,TResponse>(Topic topic,Func<T,Task<TResponse>> callbackAsync );
+        Task UnSubscribeAsync(Topic topic);
+    }
+    
+    public interface IPublishService
+    {
+        Task PublishAsync<T>(Topic topic, T payload) where T:Payload;
+    }
+
+    public class Payload
+    {
+    }
+
+    public class Topic
+    {
+    }
+
+
+    public enum AwaitOption
+    {
+        Synchronous,
+        Concurrent
+    }
+}
